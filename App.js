bfd097bb7e7c29e541b2823eb02499b366a3fdff
Matheus Castro <goca1377@gmail.com>
2018-08-19 19:01:08 -0300

    app

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 import { createStackNavigator } from 'react-navigation'
 
 import LoginScreen from './src/components/login/Login'
